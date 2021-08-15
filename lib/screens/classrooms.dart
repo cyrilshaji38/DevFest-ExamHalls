b49ff1cd5734cc72027939e40559064fc458ceb4
Cyril Shaji <cyrilshaji38@gmail.com>
2021-08-15 13:31:03 +0530

    sample classrooms

--- lib/screens/classrooms.dart
+++ lib/screens/classrooms.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
 class ClassroomsPage extends StatefulWidget {
   const ClassroomsPage({Key key}) : super(key: key);
@@ -10,6 +11,54 @@ class ClassroomsPage extends StatefulWidget {
 class _ClassroomsPageState extends State<ClassroomsPage> {
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      appBar: AppBar(title: Text("Exam Hall Booking")),
+      body: Center(
+        child: Column(children: <Widget>[
+          Text("\n\n"),
+          SizedBox(
+            width: 350,
+            height: 100,
+            child: TextButton(
+                child: Text("S6-ECE: Electronic Measurements Instrumentation"),
+                onPressed: ()=>{},
+                style: TextButton.styleFrom(
+                    primary: Colors.white,
+                    backgroundColor: Colors.purple,
+                    textStyle: TextStyle(color: Colors.white, fontSize: 20,),
+                )
+            ),
+          ),
+          Text("\n"),
+          SizedBox(
+            width: 350,
+            height: 100,
+            child: TextButton(
+                child: Text("S6-ECE: Information Theory and Coding"),
+                onPressed: ()=>{},
+                style: TextButton.styleFrom(
+                    primary: Colors.white,
+                    backgroundColor: Colors.purple,
+                    textStyle: TextStyle(color: Colors.white, fontSize: 20,)
+                )
+            ),
+          ),
+          Text("\n"),
+          SizedBox(
+            width: 350,
+            height: 100,
+            child: TextButton(
+                child: Text("S6-ECE: Microwave Theory and Techniques"),
+                onPressed: ()=>{},
+                style: TextButton.styleFrom(
+                    primary: Colors.white,
+                    backgroundColor: Colors.purple,
+                    textStyle: TextStyle(color: Colors.white, fontSize: 20,)
+                )
+            ),
+          )
+        ]),
+      )
+    );
   }
 }
